b4e1bed821f88d07ae173ded598154e63db976ce
alexprado <alexsilvadoprado@gmail.com>
2017-07-24 18:24:43 -0300

    - Adicionado imagens game

--- app/src/main/java/com/posmobile/ballohero/TelaJogo.java
+++ app/src/main/java/com/posmobile/ballohero/TelaJogo.java
@@ -2,6 +2,7 @@ package com.posmobile.ballohero;
 
 import com.posmobile.ballohero.AndGraph.AGGameManager;
 import com.posmobile.ballohero.AndGraph.AGScene;
+import com.posmobile.ballohero.AndGraph.AGScreenManager;
 import com.posmobile.ballohero.AndGraph.AGSprite;
 
 public class TelaJogo extends AGScene
@@ -15,7 +16,15 @@ public class TelaJogo extends AGScene
     }
 
     @Override
-    public void init() {}
+    public void init()
+    {
+        createSprite(R.mipmap.sprite_balao, 1, 4).bVisible = false;
+        balao = createSprite(R.mipmap.sprite_balao, 4, 1);
+        balao.setScreenPercent(20, 20);
+        balao.addAnimation(10, false, 0, 3);
+        balao.vrPosition.setX(AGScreenManager.iScreenWidth / 2);
+        balao.vrPosition.setY(balao.getSpriteHeight());
+    }
 
     @Override
     public void restart() {}
