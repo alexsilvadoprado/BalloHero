49aa9ca6925ef1dcb9f8fe74188a440e69730fe4
alexprado <alexsilvadoprado@gmail.com>
2017-08-03 17:55:22 -0300

    - Adicionado barra superior e placar

--- app/src/main/java/com/posmobile/ballohero/TelaJogo.java
+++ app/src/main/java/com/posmobile/ballohero/TelaJogo.java
@@ -15,6 +15,8 @@ public class TelaJogo extends AGScene
 {
 
     AGSprite[] fundo = new AGSprite[2];
+    AGSprite barra_superior = null;
+    AGSprite[] placar = new AGSprite[6];
     AGSprite balao = null;
 
     List<AGSprite> vetor_emojis = null;
@@ -29,6 +31,10 @@ public class TelaJogo extends AGScene
 
     AGTimer tempo_geracao_inimigos = null;
 
+    int pontuacao = 0;
+    int tempo_pontuacao = 0;
+    boolean game_over = false;
+
     TelaJogo(AGGameManager vrManager)
     {
         super(vrManager);
@@ -39,6 +45,7 @@ public class TelaJogo extends AGScene
     {
         createSprite(R.mipmap.sprite_balao, 4, 2).bVisible = false;
         createSprite(R.mipmap.sprite_emoji, 4, 1).bVisible = false;
+        createSprite(R.mipmap.barra_superior, 1, 1).bVisible = false;
 
         for(int i = 0; i < fundo.length; i++)
         {
@@ -48,6 +55,12 @@ public class TelaJogo extends AGScene
         }
         fundo[0].vrPosition.setY(AGScreenManager.iScreenHeight / 2);
         fundo[1].vrPosition.setY(AGScreenManager.iScreenHeight + fundo[1].getSpriteHeight() / 2);
+
+        barra_superior = createSprite(R.mipmap.barra_superior, 1, 1);
+        barra_superior.setScreenPercent(100, 10);
+        barra_superior.vrPosition.setX(AGScreenManager.iScreenWidth / 2);
+        barra_superior.vrPosition.setY(AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2);
+        barra_superior.bAutoRender = false;
 
         vetor_cod_inimigos[0] = R.mipmap.sprite_emoji;
         vetor_cod_inimigos[1] = R.mipmap.mamona;
@@ -67,12 +80,40 @@ public class TelaJogo extends AGScene
 
         tempo_geracao_inimigos = new AGTimer(1000);
 
+        int multiplicador = 1;
+        for(int pos = 0; pos < placar.length; pos++)
+        {
+            placar[pos] = createSprite(R.mipmap.fonte, 4, 4);
+            placar[pos].setScreenPercent(10, 10);
+            placar[pos].vrPosition.fY = barra_superior.vrPosition.fY;
+            placar[pos].vrPosition.fX = 20 + multiplicador * placar[pos].getSpriteWidth();
+            placar[pos].bAutoRender = false;
+            multiplicador++;
+
+            for(int i = 0; i < 10; i++)
+            {
+                placar[pos].addAnimation(1, true, i);
+            }
+        }
+
         balao = createSprite(R.mipmap.sprite_balao, 4, 2);
         balao.setScreenPercent(20, 20);
         balao.addAnimation(10, true, 0, 3);
         balao.addAnimation(15, false, 4, 7);
         balao.vrPosition.setX(AGScreenManager.iScreenWidth / 2);
         balao.vrPosition.setY(balao.getSpriteHeight() / 2);
+    }
+
+    @Override
+    public void render()
+    {
+        super.render();
+        barra_superior.render();
+
+        for(AGSprite digito : placar)
+        {
+            digito.render();
+        }
     }
 
     @Override
@@ -93,6 +134,8 @@ public class TelaJogo extends AGScene
         verificaToque();
 
         verificaColisao();
+
+        atualizaPlacar();
     }
 
     private void criaInimigo()
@@ -129,7 +172,7 @@ public class TelaJogo extends AGScene
                 emoji.bRecycled = false;
                 emoji.bVisible = true;
                 emoji.vrPosition.setXY(vetor_pos_inimigos[new Random().nextInt(5)],
-                        AGScreenManager.iScreenHeight + emoji.getSpriteHeight());
+                        (AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + emoji.getSpriteHeight());
 
                 prox_inimigo = 1;
 
@@ -141,7 +184,7 @@ public class TelaJogo extends AGScene
         inimigo.setScreenPercent(15, 8);
         inimigo.addAnimation(8, true, 0, 3);
         inimigo.vrPosition.setX(vetor_pos_inimigos[new Random().nextInt(5)]);
-        inimigo.vrPosition.setY(AGScreenManager.iScreenHeight + inimigo.getSpriteHeight());
+        inimigo.vrPosition.setY((AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + inimigo.getSpriteHeight());
         vetor_emojis.add(inimigo);
 
         prox_inimigo = 1;
@@ -156,7 +199,7 @@ public class TelaJogo extends AGScene
                 mamona.bRecycled = false;
                 mamona.bVisible = true;
                 mamona.vrPosition.setXY(vetor_pos_inimigos[new Random().nextInt(5)],
-                        AGScreenManager.iScreenHeight + mamona.getSpriteHeight());
+                        (AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + mamona.getSpriteHeight());
 
                 prox_inimigo = 2;
 
@@ -167,7 +210,7 @@ public class TelaJogo extends AGScene
         AGSprite inimigo = createSprite(vetor_cod_inimigos[prox_inimigo], prox_inimigo == 0 ? 4 : 1, 1);
         inimigo.setScreenPercent(15, 8);
         inimigo.vrPosition.setX(vetor_pos_inimigos[new Random().nextInt(5)]);
-        inimigo.vrPosition.setY(AGScreenManager.iScreenHeight + inimigo.getSpriteHeight());
+        inimigo.vrPosition.setY((AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + inimigo.getSpriteHeight());
         vetor_mamonas.add(inimigo);
 
         prox_inimigo = 2;
@@ -182,7 +225,7 @@ public class TelaJogo extends AGScene
                 prego.bRecycled = false;
                 prego.bVisible = true;
                 prego.vrPosition.setXY(vetor_pos_inimigos[new Random().nextInt(5)],
-                        AGScreenManager.iScreenHeight + prego.getSpriteHeight());
+                        (AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + prego.getSpriteHeight());
 
                 prox_inimigo = 3;
 
@@ -191,9 +234,9 @@ public class TelaJogo extends AGScene
         }
 
         AGSprite inimigo = createSprite(vetor_cod_inimigos[prox_inimigo], prox_inimigo == 0 ? 4 : 1, 1);
-        inimigo.setScreenPercent(8, 8);
+        inimigo.setScreenPercent(4, 8);
         inimigo.vrPosition.setX(vetor_pos_inimigos[new Random().nextInt(5)]);
-        inimigo.vrPosition.setY(AGScreenManager.iScreenHeight + inimigo.getSpriteHeight());
+        inimigo.vrPosition.setY((AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + inimigo.getSpriteHeight());
         vetor_pregos.add(inimigo);
 
         prox_inimigo = 3;
@@ -208,7 +251,7 @@ public class TelaJogo extends AGScene
                 dardo.bRecycled = false;
                 dardo.bVisible = true;
                 dardo.vrPosition.setXY(vetor_pos_inimigos[new Random().nextInt(5)],
-                        AGScreenManager.iScreenHeight + dardo.getSpriteHeight());
+                        (AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + dardo.getSpriteHeight());
 
                 prox_inimigo = 0;
 
@@ -219,7 +262,7 @@ public class TelaJogo extends AGScene
         AGSprite inimigo = createSprite(vetor_cod_inimigos[prox_inimigo], prox_inimigo == 0 ? 4 : 1, 1);
         inimigo.setScreenPercent(8, 8);
         inimigo.vrPosition.setX(vetor_pos_inimigos[new Random().nextInt(5)]);
-        inimigo.vrPosition.setY(AGScreenManager.iScreenHeight + inimigo.getSpriteHeight());
+        inimigo.vrPosition.setY((AGScreenManager.iScreenHeight - barra_superior.getSpriteHeight() / 2) + inimigo.getSpriteHeight());
         vetor_dardos.add(inimigo);
 
         prox_inimigo = 0;
@@ -232,6 +275,8 @@ public class TelaJogo extends AGScene
             emoji.vrPosition.fY -= 15;
             if(emoji.vrPosition.fY < ((emoji.getSpriteHeight() / 2) * -1))
             {
+                if(emoji.bVisible)
+                    tempo_pontuacao += 50;
                 emoji.bRecycled = true;
                 emoji.bVisible = false;
             }
@@ -242,6 +287,8 @@ public class TelaJogo extends AGScene
             mamona.vrPosition.fY -= 15;
             if(mamona.vrPosition.fY < ((mamona.getSpriteHeight() / 2) * -1))
             {
+                if(mamona.bVisible)
+                    tempo_pontuacao += 50;
                 mamona.bRecycled = true;
                 mamona.bVisible = false;
             }
@@ -252,6 +299,8 @@ public class TelaJogo extends AGScene
             prego.vrPosition.fY -= 15;
             if(prego.vrPosition.fY < ((prego.getSpriteHeight() / 2) * -1))
             {
+                if(prego.bVisible)
+                    tempo_pontuacao += 50;
                 prego.bRecycled = true;
                 prego.bVisible = false;
             }
@@ -262,6 +311,8 @@ public class TelaJogo extends AGScene
             dardo.vrPosition.fY -= 15;
             if(dardo.vrPosition.fY < ((dardo.getSpriteHeight() / 2) * -1))
             {
+                if(dardo.bVisible)
+                    tempo_pontuacao += 50;
                 dardo.bRecycled = true;
                 dardo.bVisible = false;
             }
@@ -354,5 +405,21 @@ public class TelaJogo extends AGScene
             fundo[1].vrPosition.setY(AGScreenManager.iScreenHeight + fundo[1].getSpriteHeight() / 2);
             fundo[0].vrPosition.setY(AGScreenManager.iScreenHeight / 2);
         }
+    }
+
+    private void atualizaPlacar()
+    {
+        if (tempo_pontuacao > 0)
+        {
+            tempo_pontuacao--;
+            pontuacao++;
+        }
+
+        placar[5].setCurrentAnimation(pontuacao % 10);
+        placar[4].setCurrentAnimation((pontuacao % 100) / 10);
+        placar[3].setCurrentAnimation((pontuacao % 1000) / 100);
+        placar[2].setCurrentAnimation((pontuacao % 10000) / 1000);
+        placar[1].setCurrentAnimation((pontuacao % 100000) / 10000);
+        placar[0].setCurrentAnimation(pontuacao / 100000);
     }
 }
